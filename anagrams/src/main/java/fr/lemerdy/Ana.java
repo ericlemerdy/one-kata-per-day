e036e0983cb8179af55a7026c82a54bdcfae3c74
Éric Le Merdy <eric.lemerdy@laposte.net>
2014-07-27 16:14:56 +0200

    Anagrams

---
+++ anagrams/src/main/java/fr/lemerdy/Ana.java
@@ -0,0 +1,36 @@
+package fr.lemerdy;
+
+import com.google.common.collect.Sets;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.google.common.collect.Sets.newHashSet;
+
+public class Ana {
+    public static Set<String> grams(String input) {
+        if (input.length() == 1) {
+            return newHashSet(input);
+        } else {
+            final Set<String> anagrams = newHashSet();
+            for (int i = 0; i < input.length(); i++) {
+                final String finalInput = input;
+                grams(tail(input)).stream().forEach(s -> anagrams.add(head(finalInput) + s));
+                input = permutations(input);
+            }
+            return anagrams;
+        }
+    }
+
+    protected static String permutations(String input) {
+        return tail(input) + head(input);
+    }
+
+    public static String head(String input) {
+        return input.substring(0, 1);
+    }
+
+    public static String tail(String input) {
+        return input.substring(1);
+    }
+}
