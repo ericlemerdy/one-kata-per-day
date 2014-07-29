a024c91e8b0136ccb4f60e44eb3fecbd251c46fc
elemerdy100113 <eric.le-merdy-ext@sgcib.com>
2014-07-29 13:12:15 +0200

    kata-bank-ocr

---
+++ kata-bank-OCR/src/main/java/fr/lemerdy/BankOCR.java
@@ -0,0 +1,11 @@
+package fr.lemerdy;
+
+import com.google.common.collect.Sets;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.google.common.collect.Sets.newHashSet;
+
+public class BankOCR {
+}
---
+++ kata-bank-OCR/src/test/java/fr/lemerdy/BankOCRTest.java
@@ -0,0 +1,11 @@
+package fr.lemerdy;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BankOCRTest {
+    @Test
+    public void anagrams_of_a_should_be_a() throws Exception {
+    }
+}
