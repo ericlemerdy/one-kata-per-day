8441a089312c326cee9aca476053edceeb0358ad
Eric <eric.lemerdy@laposte.net>
2014-07-30 08:23:16 +0200

    Fix tests with new Guava

--- poker-hand/src/main/java/model/Suit.java
+++ poker-hand/src/main/java/model/Suit.java
@@ -1,49 +1,49 @@
 package model;
 
-import com.google.common.base.*;
+import static com.google.common.collect.ImmutableList.copyOf;
+import static com.google.common.collect.Lists.transform;
 
-import javax.annotation.*;
-import java.util.*;
+import java.util.List;
 
-import static com.google.common.collect.ImmutableList.*;
-import static com.google.common.collect.Lists.*;
+import com.google.common.base.Function;
 
 public enum Suit {
 
-	heart('♥'), diamond('♦'), spade('♠'), club('♣');
+  heart('♥'), diamond('♦'), spade('♠'), club('♣');
 
-	private final char symbol;
+  private final char symbol;
 
-	Suit(Character symbol) {
-		this.symbol = symbol;
-	}
+  Suit(Character symbol) {
+    this.symbol = symbol;
+  }
 
-	public static Suit withChar(Character suit) {
-		final List<Character> suits = Suit.charValues();
-		if (suits.contains(suit)) {
-			for (Suit possibleSuit : Suit.values()) {
-				if (suit.equals(possibleSuit.symbol)) {
-					return possibleSuit;
-				}
-			}
-		}
-		throw new IllegalArgumentException();
-	}
+  public static Suit withChar(Character suit) {
+    final List<Character> suits = Suit.charValues();
+    if (suits.contains(suit)) {
+      for (Suit possibleSuit : Suit.values()) {
+        if (suit.equals(possibleSuit.symbol)) {
+          return possibleSuit;
+        }
+      }
+    }
+    throw new IllegalArgumentException();
+  }
 
-	protected static List<Character> charValues() {
-		return transform(copyOf(Suit.values()), new Function<Suit, Character>() {
-			@Override
-			public Character apply(@Nullable Suit suit) {
-				return suit.symbol;
-			}
-		});
-	}
+  protected static List<Character> charValues() {
+    return transform(copyOf(Suit.values()), new Function<Suit, Character>() {
+      @Override
+      public Character apply(Suit suit) {
+        return suit.symbol;
+      }
+    });
+  }
 
-	public String toString() {
-		return String.valueOf(this.symbol);
-	}
+  @Override
+  public String toString() {
+    return String.valueOf(this.symbol);
+  }
 
-	public Character toCharacter() {
-		return this.symbol;
-	}
+  public Character toCharacter() {
+    return this.symbol;
+  }
 }
