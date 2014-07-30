03baf7f1225cf59b3bc4bd3af99cf6a1f63262f3
elemerdy100113 <eric.le-merdy-ext@sgcib.com>
2014-07-30 20:08:42 +0200

    OCR first version

--- kata-bank-OCR/src/main/java/fr/lemerdy/BankOCR.java
+++ kata-bank-OCR/src/main/java/fr/lemerdy/BankOCR.java
@@ -1,11 +1,36 @@
 package fr.lemerdy;
 
-import com.google.common.collect.Sets;
+import static java.util.Arrays.asList;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import static com.google.common.collect.Sets.newHashSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BankOCR {
+
+	public static List<Integer> parse(String input) {
+		String[] split = input.split("\n");
+		List<Integer> output = new ArrayList<Integer>();
+		output.addAll(parse(split[0].substring(0), split[1].substring(0),
+				split[2].substring(0)));
+		if (split[0].length() > 3) {
+			output.addAll(parse(split[0].substring(3), split[1].substring(3),
+					split[2].substring(3)));
+		}
+		if (split[0].length() > 6) {
+			output.addAll(parse(split[0].substring(6), split[1].substring(6),
+					split[2].substring(6)));
+		}
+		return output;
+	}
+
+	public static List<Integer> parse(String line0, String line1, String line2) {
+		if (line0.charAt(1) == ' ') {
+			return asList(1);
+		}
+		if (line2.charAt(2) == ' ') {
+			return asList(2);
+		}
+		return asList(3);
+	}
+
 }
--- kata-bank-OCR/src/test/java/fr/lemerdy/BankOCRTest.java
+++ kata-bank-OCR/src/test/java/fr/lemerdy/BankOCRTest.java
@@ -1,11 +1,56 @@
 package fr.lemerdy;
 
-import org.junit.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
+import org.junit.Test;
+
 public class BankOCRTest {
-    @Test
-    public void anagrams_of_a_should_be_a() throws Exception {
-    }
+	@Test
+	public void should_parse_the_number_one() {
+		List<Integer> parsedInput = BankOCR.parse("" //
+				+ "   \n" //
+				+ "  |\n" //
+				+ "  |");
+		assertThat(parsedInput).containsExactly(1);
+	}
+
+	@Test
+	public void should_parse_the_number_two() {
+		List<Integer> parsedInput = BankOCR.parse("" //
+				+ " _ \n" //
+				+ " _|\n" //
+				+ "|_ ");
+		assertThat(parsedInput).containsExactly(2);
+	}
+
+	@Test
+	public void should_parse_three() {
+		List<Integer> parsedInput = BankOCR.parse("" //
+				+ " _ \n" //
+				+ " _|\n" //
+				+ " _|");
+		assertThat(parsedInput).containsExactly(3);
+	}
+
+	@Test
+	public void should_parse_2_numbers() {
+		List<Integer> parsedInput = BankOCR.parse("" //
+				+ "      \n" //
+				+ "  |  |\n" //
+				+ "  |  |");
+		assertThat(parsedInput).containsExactly(1, 1);
+
+	}
+
+	@Test
+	public void should_parse_3_numbers() {
+		List<Integer> parsedInput = BankOCR.parse("" //
+				+ "         \n" //
+				+ "  |  |  |\n" //
+				+ "  |  |  |");
+		assertThat(parsedInput).containsExactly(1, 1, 1);
+
+	}
 }
