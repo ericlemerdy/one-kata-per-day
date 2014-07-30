687764c6268d6ab4764411dec88d3d42ba98ee08
Eric <eric.lemerdy@laposte.net>
2014-07-30 07:29:17 +0200

    Fibonacci

---
+++ fibonacci/README.md
@@ -0,0 +1,12 @@
+Fibonacci
+=========
+
+## Mathematial definition
+
+f(i) = f(i-2) + f(i-1)
+
+with f(0) = 1 and f(1) = 1
+
+## Exemple
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
---
+++ fibonacci/src/main/java/fr/lemerdy/Fibonacci.java
@@ -0,0 +1,26 @@
+package fr.lemerdy;
+
+import static com.google.common.collect.Lists.newArrayList;
+
+import java.util.List;
+
+public class Fibonacci {
+
+  public static int of(int i) {
+    if (i == 0) {
+      return 1;
+    }
+    if (i == 1) {
+      return 1;
+    }
+    return Fibonacci.of(i - 2) + Fibonacci.of(i - 1);
+  }
+
+  public static List<Integer> sequenceUpTo(int limit) {
+    List<Integer> sequence = newArrayList();
+    for (int i = 0; i <= limit; i++) {
+      sequence.add(Fibonacci.of(i));
+    }
+    return sequence;
+  }
+}
