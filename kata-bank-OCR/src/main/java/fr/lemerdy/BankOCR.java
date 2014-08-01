48b2658aa1d495fed7ec4f3ddfdd80aed3b1ddea
elemerdy100113 <eric.le-merdy-ext@sgcib.com>
2014-08-01 10:57:04 +0200

    refacto OCR

--- kata-bank-OCR/src/main/java/fr/lemerdy/BankOCR.java
+++ kata-bank-OCR/src/main/java/fr/lemerdy/BankOCR.java
@@ -1,31 +1,69 @@
 package fr.lemerdy;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.collect.Lists.newArrayList;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class BankOCR {
 
-	public static List<Integer> parse(String input) {
+	public static List<Integer> forEntry(String input) {
 		String[] split = input.split("\n");
+		checkArgument(split.length == 4, "Expecting 4 lines for an entry");
+		newArrayList(split).stream().forEach(
+				s -> checkArgument(s.length() == 27,
+						"Expecting 27 characters per line for an entry"));
+		return BankOCR.parse(split[0], split[1], split[2]);
+	}
+
+	public static List<Integer> parse(String line0, String line1, String line2) {
 		List<Integer> output = new ArrayList<Integer>();
-		for (int i = 0; i < split[0].length(); i+=3) {
-			output.add(parse(split[0].substring(i),
-					split[1].substring(i), split[2].substring(i)));
+		for (int i = 0; i < line0.length(); i += 3) {
+			output.add(parseNumber(line0.substring(i), line1.substring(i),
+					line2.substring(i)));
 		}
 		return output;
 	}
 
-	public static int parse(String line0, String line1, String line2) {
-		if (line1.charAt(0) != ' ') {
-			return 4;
-		}
+	public static int parseNumber(String line0, String line1, String line2) {
 		if (line0.charAt(1) == ' ') {
-			return 1;
+			if (line1.charAt(0) == ' ') {
+				return 1;
+			} else {
+				return 4;
+			}
+		} else {
+			if (line1.charAt(0) == ' ') {
+				if (line1.charAt(1) == ' ') {
+					return 7;
+				} else {
+					if (line2.charAt(0) == ' ') {
+						return 3;
+					} else {
+						return 2;
+					}
+				}
+			} else {
+				if (line1.charAt(2) == ' ') {
+					if (line2.charAt(0) == ' ') {
+						return 5;
+					} else {
+						return 6;
+					}
+				} else {
+					if (line2.charAt(0) == ' ') {
+						return 9;
+					} else {
+						if (line1.charAt(1) == ' ') {
+							return 0;
+						} else {
+							return 8;
+						}
+					}
+				}
+			}
 		}
-		if (line2.charAt(2) == ' ') {
-			return 2;
-		}
-		return 3;
 	}
 
 }
--- kata-bank-OCR/src/test/java/fr/lemerdy/BankOCRTest.java
+++ kata-bank-OCR/src/test/java/fr/lemerdy/BankOCRTest.java
@@ -1,65 +1,156 @@
 package fr.lemerdy;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
 
 import java.util.List;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class BankOCRTest {
+
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+
+	@Test
+	public void should_parse_the_number_zero() {
+		int parsedInput = BankOCR.parseNumber( //
+				" _ ", //
+				"| |", //
+				"|_|");
+		assertThat(parsedInput).isEqualTo(0);
+	}
+
 	@Test
 	public void should_parse_the_number_one() {
-		List<Integer> parsedInput = BankOCR.parse("" //
-				+ "   \n" //
-				+ "  |\n" //
-				+ "  |");
-		assertThat(parsedInput).containsExactly(1);
+		int parsedInput = BankOCR.parseNumber( //
+				"   ", //
+				"  |", //
+				"  |");
+		assertThat(parsedInput).isEqualTo(1);
 	}
 
 	@Test
 	public void should_parse_the_number_two() {
-		List<Integer> parsedInput = BankOCR.parse("" //
-				+ " _ \n" //
-				+ " _|\n" //
-				+ "|_ ");
-		assertThat(parsedInput).containsExactly(2);
+		int parsedInput = BankOCR.parseNumber( //
+				" _ ", //
+				" _|", //
+				"|_ ");
+		assertThat(parsedInput).isEqualTo(2);
 	}
 
 	@Test
-	public void should_parse_three() {
-		List<Integer> parsedInput = BankOCR.parse("" //
-				+ " _ \n" //
-				+ " _|\n" //
-				+ " _|");
-		assertThat(parsedInput).containsExactly(3);
+	public void should_parse_the_number_three() {
+		int parsedInput = BankOCR.parseNumber( //
+				" _ ", //
+				" _|", //
+				" _|");
+		assertThat(parsedInput).isEqualTo(3);
 	}
 
 	@Test
-	public void should_parse_four() {
-		List<Integer> parsedInput = BankOCR.parse("" //
-				+ "   \n" //
-				+ "|_|\n" //
-				+ "  |");
-		assertThat(parsedInput).containsExactly(4);
+	public void should_parse_the_number_four() {
+		int parsedInput = BankOCR.parseNumber( //
+				"   ", //
+				"|_|", //
+				"  |");
+		assertThat(parsedInput).isEqualTo(4);
+	}
+
+	@Test
+	public void should_parse_the_number_five() {
+		int parsedInput = BankOCR.parseNumber( //
+				" _ ", //
+				"|_ ", //
+				" _|");
+		assertThat(parsedInput).isEqualTo(5);
+	}
+
+	@Test
+	public void should_parse_the_number_six() {
+		int parsedInput = BankOCR.parseNumber( //
+				" _ ", //
+				"|_ ", //
+				"|_|");
+		assertThat(parsedInput).isEqualTo(6);
+	}
+
+	@Test
+	public void should_parse_the_number_seven() {
+		int parsedInput = BankOCR.parseNumber( //
+				" _ ", //
+				"  |", //
+				"  |");
+		assertThat(parsedInput).isEqualTo(7);
+	}
+
+	@Test
+	public void should_parse_the_number_eight() {
+		int parsedInput = BankOCR.parseNumber( //
+				" _ ", //
+				"|_|", //
+				"|_|");
+		assertThat(parsedInput).isEqualTo(8);
+	}
+
+	@Test
+	public void should_parse_the_number_nine() {
+		int parsedInput = BankOCR.parseNumber( //
+				" _ ", //
+				"|_|", //
+				" _|");
+		assertThat(parsedInput).isEqualTo(9);
 	}
 
 	@Test
 	public void should_parse_2_numbers() {
-		List<Integer> parsedInput = BankOCR.parse("" //
-				+ "      \n" //
-				+ "  |  |\n" //
-				+ "  |  |");
+		List<Integer> parsedInput = BankOCR.parse( //
+				"      ", //
+				"  |  |", //
+				"  |  |");
 		assertThat(parsedInput).containsExactly(1, 1);
 
 	}
 
 	@Test
 	public void should_parse_3_numbers() {
-		List<Integer> parsedInput = BankOCR.parse("" //
-				+ "         \n" //
-				+ "  |  |  |\n" //
-				+ "  |  |  |");
+		List<Integer> parsedInput = BankOCR.parse( //
+				"         ", //
+				"  |  |  |", //
+				"  |  |  |");
 		assertThat(parsedInput).containsExactly(1, 1, 1);
 
+	}
+
+	@Test
+	public void should_parse_an_entry() {
+		List<Integer> parsedInput = BankOCR.forEntry("" //
+				+ "    _  _     _  _  _  _  _ \n" //
+				+ "  | _| _||_||_ |_   ||_||_|\n" //
+				+ "  ||_  _|  | _||_|  ||_| _|\n" //
+				+ "                           ");
+		assertThat(parsedInput).containsExactly(1, 2, 3, 4, 5, 6, 7, 8, 9);
+	}
+
+	@Test
+	public void an_entry_with_27_character_but_2_lines_is_invalid() {
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage(containsString("Expecting 4 lines for an entry"));
+		BankOCR.forEntry("" //
+				+ "                           \n" //
+				+ "                           ");
+	}
+
+	@Test
+	public void an_entry_with_4_lines_but_26_characters_is_invalid() {
+		exception.expect(IllegalArgumentException.class);
+		exception.expectMessage(containsString("Expecting 27 characters per line for an entry"));
+		BankOCR.forEntry("" //
+				+ "                          \n" //
+				+ " \n" //
+				+ " \n" //
+				+ " ");
 	}
 }
