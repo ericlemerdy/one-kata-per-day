735bcd5c4e5ec3deaa83231ee872f40011621b33
elemerdy100113 <eric.le-merdy-ext@sgcib.com>
2014-08-14 19:08:27 +0200

    Refactor with specific classes

--- kata-bank-OCR/src/test/java/fr/lemerdy/BankOCRTest.java
+++ kata-bank-OCR/src/test/java/fr/lemerdy/BankOCRTest.java
@@ -15,130 +15,20 @@ public class BankOCRTest {
 	public ExpectedException exception = ExpectedException.none();
 
 	@Test
-	public void should_parse_the_number_zero() {
-		int parsedInput = BankOCR.parseNumber( //
-				" _ ", //
-				"| |", //
-				"|_|");
-		assertThat(parsedInput).isEqualTo(0);
-	}
-
-	@Test
-	public void should_parse_the_number_one() {
-		int parsedInput = BankOCR.parseNumber( //
-				"   ", //
-				"  |", //
-				"  |");
-		assertThat(parsedInput).isEqualTo(1);
-	}
-
-	@Test
-	public void should_parse_the_number_two() {
-		int parsedInput = BankOCR.parseNumber( //
-				" _ ", //
-				" _|", //
-				"|_ ");
-		assertThat(parsedInput).isEqualTo(2);
-	}
-
-	@Test
-	public void should_parse_the_number_three() {
-		int parsedInput = BankOCR.parseNumber( //
-				" _ ", //
-				" _|", //
-				" _|");
-		assertThat(parsedInput).isEqualTo(3);
-	}
-
-	@Test
-	public void should_parse_the_number_four() {
-		int parsedInput = BankOCR.parseNumber( //
-				"   ", //
-				"|_|", //
-				"  |");
-		assertThat(parsedInput).isEqualTo(4);
-	}
-
-	@Test
-	public void should_parse_the_number_five() {
-		int parsedInput = BankOCR.parseNumber( //
-				" _ ", //
-				"|_ ", //
-				" _|");
-		assertThat(parsedInput).isEqualTo(5);
-	}
-
-	@Test
-	public void should_parse_the_number_six() {
-		int parsedInput = BankOCR.parseNumber( //
-				" _ ", //
-				"|_ ", //
-				"|_|");
-		assertThat(parsedInput).isEqualTo(6);
-	}
-
-	@Test
-	public void should_parse_the_number_seven() {
-		int parsedInput = BankOCR.parseNumber( //
-				" _ ", //
-				"  |", //
-				"  |");
-		assertThat(parsedInput).isEqualTo(7);
-	}
-
-	@Test
-	public void should_parse_the_number_eight() {
-		int parsedInput = BankOCR.parseNumber( //
-				" _ ", //
-				"|_|", //
-				"|_|");
-		assertThat(parsedInput).isEqualTo(8);
-	}
-
-	@Test
-	public void should_parse_the_number_nine() {
-		int parsedInput = BankOCR.parseNumber( //
-				" _ ", //
-				"|_|", //
-				" _|");
-		assertThat(parsedInput).isEqualTo(9);
-	}
-
-	@Test
-	public void should_parse_2_numbers() {
-		List<Integer> parsedInput = BankOCR.parse( //
-				"      ", //
-				"  |  |", //
-				"  |  |");
-		assertThat(parsedInput).containsExactly(1, 1);
-
-	}
-
-	@Test
-	public void should_parse_3_numbers() {
-		List<Integer> parsedInput = BankOCR.parse( //
-				"         ", //
-				"  |  |  |", //
-				"  |  |  |");
-		assertThat(parsedInput).containsExactly(1, 1, 1);
-
-	}
-
-	@Test
 	public void should_parse_an_entry() {
-		List<Integer> parsedInput = BankOCR.forEntry("" //
+		Entry parsedInput = BankOCR.entry("" //
 				+ "    _  _     _  _  _  _  _ \n" //
 				+ "  | _| _||_||_ |_   ||_||_|\n" //
 				+ "  ||_  _|  | _||_|  ||_| _|\n" //
 				+ "                           ");
-		assertThat(parsedInput).containsExactly(1, 2, 3, 4, 5, 6, 7, 8, 9);
+		assertThat(parsedInput.input()).containsExactly(1, 2, 3, 4, 5, 6, 7, 8, 9);
 	}
 
 	@Test
 	public void an_entry_with_27_character_but_2_lines_is_invalid() {
 		exception.expect(IllegalArgumentException.class);
 		exception.expectMessage(containsString("Expecting 4 lines for an entry"));
-		BankOCR.forEntry("" //
+		BankOCR.entry("" //
 				+ "                           \n" //
 				+ "                           ");
 	}
@@ -147,10 +37,22 @@ public class BankOCRTest {
 	public void an_entry_with_4_lines_but_26_characters_is_invalid() {
 		exception.expect(IllegalArgumentException.class);
 		exception.expectMessage(containsString("Expecting 27 characters per line for an entry"));
-		BankOCR.forEntry("" //
+		BankOCR.entry("" //
 				+ "                          \n" //
 				+ " \n" //
 				+ " \n" //
 				+ " ");
+	}
+	
+	@Test
+	public void should_create_an_entry() throws Exception {
+		Entry entry = BankOCR.entry("" //
+				+ "    _  _     _  _  _  _  _ \n" //
+				+ "  | _| _||_||_ |_   ||_||_|\n" //
+				+ "  ||_  _|  | _||_|  ||_| _|\n" //
+				+ "                           ");
+		List<Integer> account = entry.input();
+		assertThat(account).containsExactly(1,2,3,4,5,6,7,8,9);
+		
 	}
 }
